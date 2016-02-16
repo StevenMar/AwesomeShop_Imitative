fc9e9d54ede53b1996e1197c8a559ad5923d256f
Roy Sun <roy1993sun@gmail.com>
2016-02-16 15:54:12 +0800

    Initial commit, create utils.

---
+++ app/src/test/java/com/roy_sun/awesomeshop_imitative/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.roy_sun.awesomeshop_imitative;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
